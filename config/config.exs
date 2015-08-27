ffabf9bbbb92677fa8099a259ece90cf0886ca58
Michael Alekseev <justmichael@mail.ru>
2015-08-27 22:24:20 +0600

    ignore /deps

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :recaptcha, :api_config,
                  %{ verify_url: "https://www.google.com/recaptcha/api/siteverify",
-                    public_key: "YOUR PUBLIC KEY",
-                    private_key: "YOUR PRIVATE KEY"
+                    public_key: "YOUR_PUBLIC_KEY",
+                    private_key: "YOUR_PRIVATE_KEY"
                   }
