fc94f0319713eb306e1c47100edbfa78f27ea0a7
Michael <Justmichael@mail.ru>
2015-08-29 12:47:36 +0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Recaptcha.Mixfile do
 
   def project do
     [app: :recaptcha,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.0.0",
      description: description,
      deps: deps,
