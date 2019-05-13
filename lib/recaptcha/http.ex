a01214990685a219776795b8c8f9938894a82f70
Herman verschooten <Herman@verschooten.net>
2019-05-13 08:38:25 +0200

    Rely on config to provide the default JSON decoder

--- lib/recaptcha/http.ex
+++ lib/recaptcha/http.ex
@@ -39,7 +39,7 @@ defmodule Recaptcha.Http do
   def request_verification(body, options \\ []) do
     timeout = options[:timeout] || Config.get_env(:recaptcha, :timeout, 5000)
     url = Config.get_env(:recaptcha, :verify_url, @default_verify_url)
-    json = Application.get_env(:recaptcha, :json_library, Poison)
+    json = Application.get_env(:recaptcha, :json_library)
 
     result =
       with {:ok, response} <- HTTPoison.post(url, body, @headers, timeout: timeout),
